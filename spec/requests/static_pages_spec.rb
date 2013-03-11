6774993a76a3199a2964133cf1f685a85aebd6f0
kiv <aah@gmx.net>
2013-03-11 18:20:29 +0100

    Resized static Code (Layouted)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,24 +1,38 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  describe "Home page" do
-    it "should have the content 'Sample App'" do
-	    visit '/static_pages/home'
-	    page.should have_content('Sample App')
-    end
-  end
-
-describe "Help page" do
-	it "should have the content 'Help'" do
-		visit '/static_pages/help'
-		page.should have_content('Help')
+	describe "Home page" do
+		it "should have the content 'Sample App'" do
+			visit '/static_pages/home'
+			page.should have_content('Sample App')
+		end
+		it "should have the right title" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text => "Sample Application | Home")
+		end
 	end
-    end
 
-describe "About page" do
-	it "should have the content 'About Us'" do
-		visit '/static_pages/about'
-		page.should have_content('About Us')
+
+	describe "Help page" do
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			page.should have_content('Help')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/help'
+			page.should have_selector('title', :text => "Sample Application | Help")
+		end
 	end
-    end
+	describe "About page" do
+		it "should have the content 'About Us'" do
+			visit '/static_pages/about'
+			page.should have_content('About Us')
+		end 
+		it "should have the right title" do
+			visit '/static_pages/about'
+			page.should have_selector('title', :text => "Sample Application | About")
+		end
+	end
+
 end
